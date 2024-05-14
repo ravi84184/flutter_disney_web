b78375c7633526acd8566b6eb288d28022a43c61
Ravi Patel <ravi.patel@otuindia.com>
2024-05-14 10:20:51 +0530

    -- main body movie listing

--- lib/feature/dashboard/dashboard_screen.dart
+++ lib/feature/dashboard/dashboard_screen.dart
@@ -1,5 +1,7 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_disney_web/feature/dashboard/widgets/app_bar_widget.dart';
+import 'package:flutter_disney_web/utils/app_image.dart';
 
 import 'widgets/side_bar_widget.dart';
 
@@ -33,10 +35,72 @@ class _DashBoardScreenState extends State<DashBoardScreen> {
                   const AppBarWidget(),
                   Expanded(
                     child: Container(
-                      color: const Color(0xff121212),
+                      // color: const Color(0xff121212),
+                      color: Colors.white,
+                      child: Column(
+                        mainAxisSize: MainAxisSize.min,
+                        children: [
+                          SingleChildScrollView(
+                            scrollDirection: Axis.horizontal,
+                            child: Row(
+                              mainAxisSize: MainAxisSize.min,
+                              children: [
+                                for (var model in ['', '', '', '', '']) ...[
+                                  Container(
+                                    margin: EdgeInsets.symmetric(
+                                        horizontal: 5, vertical: 10),
+                                    child: ClipRRect(
+                                      borderRadius: BorderRadius.circular(10),
+                                      child: Stack(
+                                        children: [
+                                          Image.asset(
+                                            AppImage.mov4,
+                                            height: 250,
+                                            width: 200,
+                                            fit: BoxFit.cover,
+                                          ),
+                                          Container(
+                                            height: 250,
+                                            width: 200,
+                                            decoration: BoxDecoration(
+                                              gradient: LinearGradient(
+                                                  colors: [
+                                                    const Color(0xFF192037)
+                                                        .withOpacity(0.3),
+                                                    const Color(0xFF131418),
+                                                  ],
+                                                  begin: const FractionalOffset(
+                                                      0.0, 0.0),
+                                                  end: const FractionalOffset(
+                                                      0.0, 1.0),
+                                                  tileMode: TileMode.clamp),
+                                            ),
+                                          ),
+                                          Positioned(
+                                            right: 10,
+                                            top: 5,
+                                            child: Text(
+                                              "Sci-Fi",
+                                              style: TextStyle(
+                                                  fontSize: 16,
+                                                  color: Color(0xffF9F9F9)
+                                                      .withOpacity(0.7)),
+                                            ),
+                                          )
+                                        ],
+                                      ),
+                                    ),
+                                  ),
+                                ]
+                              ],
+                            ),
+                          )
+                        ],
+                      ),
                     ),
                   ),
-              ],),
+                ],
+              ),
             ),
           )
         ],
--- lib/utils/app_image.dart
+++ lib/utils/app_image.dart
@@ -7,5 +7,11 @@ class AppImage {
   static String icTopRated = "assets/ic_top_rated.png";
   static String icWatchlist = "assets/ic_watchlist.png";
   static String icNotification = "assets/ic_notification.png";
-  static String icSearch = "assets/ic_search.png";  
+  static String icSearch = "assets/ic_search.png";
+
+  static String mov1 = "assets/1.jpg";
+  static String mov2 = "assets/2.jpg";
+  static String mov3 = "assets/3.jpg";
+  static String mov4 = "assets/4.jpg";
+  static String mov5 = "assets/5.jpg";
 }
